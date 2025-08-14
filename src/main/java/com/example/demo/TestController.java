8dc1d014adfb5c924f68da7a82691177e255a0ad
sakshik-23 <sakshikokare2304@gmail.com>
2025-08-14 16:05:24 +0530

    Commiting from git

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping(value = "/")
+    public String forward() {
+        return "forward:/index.html";
+    }
+
+}
